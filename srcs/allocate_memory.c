20ff721c06087feaccd43cf88a8b88c8033ab883
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2022-12-19 15:03:52 +0100

    new file for function to allocate memory for structs

---
+++ srcs/allocate_memory.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   allocate_memory.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/16 14:43:44 by kczichow          #+#    #+#             */
+/*   Updated: 2022/12/19 15:03:02 by kczichow         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+/* ALLOCATE_MEMORY
+* ------------------
+* allocate memory for all structs within t_image struct
+*/
+
+void	allocate_memory(t_image *image)
+{	
+	image->var = ft_calloc(1, sizeof(t_var));
+	if (!image->var)
+		return ;
+	image->pixel = ft_calloc(1, sizeof(t_pixel));
+	if (!image->pixel)
+		return ;
+	image->coordinates = ft_calloc(1, sizeof(t_coordinates));
+	if (!image->coordinates)
+		return ;
+}
