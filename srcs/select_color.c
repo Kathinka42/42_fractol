cdbff3233c0888dac18fad3ae9d60e1ff69e2f36
Kathrin Czichowsky <kczichow@3-B-11.42heilbronn.de>
2022-12-22 15:05:58 +0100

    file modified, but new functions not working yet

--- srcs/select_color.c
+++ srcs/select_color.c
@@ -6,11 +6,24 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 11:46:38 by kczichow          #+#    #+#             */
-/*   Updated: 2022/12/20 13:39:33 by kczichow         ###   ########.fr       */
+/*   Updated: 2022/12/22 14:39:26 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+int	get_color(int n)
+{
+	int color[10];
+
+	color[0] = 0x00F08080;
+	
+}
+
+// int get_rgba(int r, int g, int b, int a)
+// {
+//     return (r << 24 | g << 16 | b << 8 | a);
+// }
 
 
 int	select_color(int iter, t_image *image)
