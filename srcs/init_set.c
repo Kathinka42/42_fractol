b1ec8bfe0e5be4261bee9d57892143711ca6cbf0
Kathrin Czichowsky <kczichow@3-B-11.42heilbronn.de>
2022-12-22 15:04:46 +0100

    new variables also need to be initialized

--- srcs/init_set.c
+++ srcs/init_set.c
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 14:40:16 by kczichow          #+#    #+#             */
-/*   Updated: 2022/12/20 15:25:08 by kczichow         ###   ########.fr       */
+/*   Updated: 2022/12/22 14:33:15 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,6 @@ void	init_set(t_set *set)
 	set->max_real = 2.0;
 	set->min_imag = -2.0;
 	set->min_real = -2.0;
+	// set->new_height = set->max_imag - set->min_imag;
+	// set->new_width = set->max_real - set->min_real;
 }
