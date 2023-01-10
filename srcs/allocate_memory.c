9c457289555dd03280ee54702b2143bf009dc0f4
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-10 15:14:12 +0100

    removed pixel struct, because not needed anymore

--- srcs/allocate_memory.c
+++ srcs/allocate_memory.c
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 14:43:44 by kczichow          #+#    #+#             */
-/*   Updated: 2022/12/19 17:40:06 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/10 14:38:48 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,6 @@
 
 void	allocate_memory(t_image *image)
 {	
-	image->pixel = ft_calloc(1, sizeof(t_pixel));
-	if (!image->pixel)
-		return ;
 	image->coordinates = ft_calloc(1, sizeof(t_coordinates));
 	if (!image->coordinates)
 		return ;
