b5c2df907910cee9773f4e1e43b8cb19aada77d7
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-10 15:27:08 +0100

    play files

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 10:56:33 by kczichow          #+#    #+#             */
-/*   Updated: 2022/12/20 12:02:38 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/10 10:34:04 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,6 @@
 #include <math.h>
 
 static mlx_image_t	*g_img;
-
-int get_rgba(int r, int g, int b, int a)
-{
-    return (r << 24 | g << 16 | b << 8 | a);
-}
 
 void my_keyhook(mlx_key_data_t keydata, void *param)
 {
@@ -41,6 +36,43 @@ void my_keyhook(mlx_key_data_t keydata, void *param)
 	
 	if (keydata.key == MLX_KEY_A && keydata.action == MLX_RELEASE && keydata.modifier == MLX_CONTROL)
 		puts("Gotta grab it all!");
+}
+
+// void my_scrollhook(double xdelta, double ydelta, t_image *image)
+// {
+// 	// Simple up or down detection.
+// 	if (ydelta > 0)
+// 		puts("Up!");
+// 		// g_img->instances[0].y -= 5;
+// 		// g_img->instances[0] -= g_img->instances * 0.9;
+
+// 	else if (ydelta < 0)
+// 		puts("Down!");
+	
+// 	// Can also detect a mousewheel that go along the X (e.g: MX Master 3)
+// 	if (xdelta < 0)
+// 		puts("Sliiiide to the left!");
+// 	else if (xdelta > 0)
+// 		puts("Sliiiide to the right!");
+// }
+
+// left side key = 0;
+// right side key = 1;
+// wheel key = 2;
+// for scrolling use scrollhook
+// int mouse_key, int action, int modifier_key, 
+void	my_mousehook(mouse_key_t button, action_t action, modifier_key_t mods, mlx_t *mlx)
+{
+	int x;
+	int y;
+	
+	if (button == 0 && action == MLX_PRESS)
+		printf("Hello from mouse_hook!\n");
+	if (button == 0 && action == MLX_RELEASE)
+		mlx_get_mouse_pos(mlx, &x, &y);
+		// printf("Hello from mouse_hook!\n");
+	if (button == 1)
+		printf("Hey\n");
 }
 
 void	hook(void *param)
@@ -64,20 +96,21 @@ void	hook(void *param)
 
 //Find out how the memset function works in this context;
 
-// int32_t	main(void)
+// int32_t	main (void)
 // {
 // 	mlx_t	*mlx;
-
+	
 // 	mlx = mlx_init(WIDTH, HEIGHT, "My first window", true);
 // 	if (!mlx)
 // 		exit(EXIT_FAILURE);
 // 	mlx_key_hook(mlx, &my_keyhook, NULL);
-// 	mlx_scroll_hook(mlx, &mouse_scroll, NULL);
+// 	mlx_mouse_hook(mlx, &my_mousehook, NULL);
+// 	mlx_scroll_hook(mlx, &my_scrollhook, NULL);
 // 	g_img = mlx_new_image(mlx, 800, 800);
 // 	memset(g_img->pixels, 255, g_img->width * g_img->height * sizeof(int32_t));
 	
 // 	draw_line(g_img, mlx);
-	
+
 // 	//mlx_put_pixel(g_img, 20 , 20, get_rgba(153, 150, 150 , 0));
 	
 // 	mlx_loop_hook(mlx, &hook, mlx);
@@ -86,4 +119,18 @@ void	hook(void *param)
 // 	//mandelbrot();
 // 	mlx_terminate(mlx);
 // 	return (EXIT_SUCCESS);
+// }
+
+// int main (int argc, char **argv)
+// {
+	// t_image *image;
+
+	// image = malloc(sizeof(t_image));
+	// if (!image)
+	// 	return (0);
+	// allocate_memory(image);
+	// init_set(image->set);
+	// fractol(argv, image);
+	// select_color(21, image);
+// 	return (0);
 // }
--- srcs/temp_drawing_experiments.c
+++ srcs/temp_drawing_experiments.c
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 10:56:33 by kczichow          #+#    #+#             */
-/*   Updated: 2022/12/16 14:02:21 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/10 10:02:30 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,14 @@
 
 void	draw_circle(mlx_image_t *g_img, mlx_t *mlx, int xc, int yc, int x, int y)
 {
-	mlx_put_pixel(g_img, xc+x , yc+y, get_rgba(0, 0, 255 , 0));
-	mlx_put_pixel(g_img, xc-x , yc+y, get_rgba(0, 0, 255 , 0));
-    mlx_put_pixel(g_img, xc+x , yc-y, get_rgba(0, 0, 255 , 0));
-    mlx_put_pixel(g_img, xc-x , yc-y, get_rgba(0, 0, 255 , 0));
-    mlx_put_pixel(g_img, xc+y , yc+x, get_rgba(0, 0, 255 , 0));
-    mlx_put_pixel(g_img, xc-y , yc+x, get_rgba(0, 0, 255 , 0));
-    mlx_put_pixel(g_img, xc+y , yc-x, get_rgba(0, 0, 255 , 0));
-    mlx_put_pixel(g_img, xc-y , yc-y, get_rgba(0, 0, 255 , 0));
+	mlx_put_pixel(g_img, xc+x , yc+y, get_rgba(0, 0, 255));
+	mlx_put_pixel(g_img, xc-x , yc+y, get_rgba(0, 0, 255));
+    mlx_put_pixel(g_img, xc+x , yc-y, get_rgba(0, 0, 255));
+    mlx_put_pixel(g_img, xc-x , yc-y, get_rgba(0, 0, 255));
+    mlx_put_pixel(g_img, xc+y , yc+x, get_rgba(0, 0, 255));
+    mlx_put_pixel(g_img, xc-y , yc+x, get_rgba(0, 0, 255));
+    mlx_put_pixel(g_img, xc+y , yc-x, get_rgba(0, 0, 255));
+    mlx_put_pixel(g_img, xc-y , yc-y, get_rgba(0, 0, 255));
 }
 
 void	circleBres(mlx_image_t *g_img, mlx_t *mlx)
@@ -77,12 +77,12 @@ void	draw_square(mlx_image_t	*g_img, mlx_t *mlx, int x, int y)
 	
 	while(x > (i - 10))
 	{
-		mlx_put_pixel(g_img, x , y, get_rgba(0, 0, 255 , 0));
+		mlx_put_pixel(g_img, x , y, get_rgba(0, 0, 255));
 		x--;
 	}
 	while(y > (j - 10))
 	{
-		mlx_put_pixel(g_img, x , y, get_rgba(20, 20, 0 , 0));
+		mlx_put_pixel(g_img, x , y, get_rgba(20, 20, 0));
 		y--;
 	}
 	printf("x is: %d\n", x);
@@ -100,14 +100,14 @@ void drawcircle(mlx_image_t	*g_img, mlx_t *mlx)
     mlx_image_to_window(mlx, g_img, 100, 100);
     while (x >= y)
     {
-        mlx_put_pixel(g_img, x0 + x , y0 + y, get_rgba(0, 0, 255 , 0));
-        mlx_put_pixel(g_img, x0 + y , y0 + x, get_rgba(0, 0, 255 , 0));
-        mlx_put_pixel(g_img, x0 - y , y0 + x, get_rgba(0, 0, 255 , 0));
-        mlx_put_pixel(g_img, x0 - x , y0 + y, get_rgba(0, 0, 255 , 0));
-        mlx_put_pixel(g_img, x0 - x , y0 - y, get_rgba(0, 0, 255 , 0));
-        mlx_put_pixel(g_img, x0 - y , y0 - x, get_rgba(0, 0, 255 , 0));
-        mlx_put_pixel(g_img, x0 + y , y0 - x, get_rgba(0, 0, 255 , 0));
-        mlx_put_pixel(g_img, x0 + x , y0 - y, get_rgba(0, 0, 255 , 0));
+        mlx_put_pixel(g_img, x0 + x , y0 + y, get_rgba(0, 0, 255));
+        mlx_put_pixel(g_img, x0 + y , y0 + x, get_rgba(0, 0, 255));
+        mlx_put_pixel(g_img, x0 - y , y0 + x, get_rgba(0, 0, 255));
+        mlx_put_pixel(g_img, x0 - x , y0 + y, get_rgba(0, 0, 255));
+        mlx_put_pixel(g_img, x0 - x , y0 - y, get_rgba(0, 0, 255));
+        mlx_put_pixel(g_img, x0 - y , y0 - x, get_rgba(0, 0, 255));
+        mlx_put_pixel(g_img, x0 + y , y0 - x, get_rgba(0, 0, 255));
+        mlx_put_pixel(g_img, x0 + x , y0 - y, get_rgba(0, 0, 255));
         
 if (err <= 0)
 {
