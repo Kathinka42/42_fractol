09529ddac266790c0d37d06cacbbaefa91c1c8dc
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-10 15:26:23 +0100

    hooks still to be added

--- srcs/fractol.c
+++ srcs/fractol.c
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 10:54:21 by kczichow          #+#    #+#             */
-/*   Updated: 2022/12/20 15:57:49 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/10 15:25:33 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,19 @@ void	fractol(char **argv, t_image *image)
 	memset(image->g_img->pixels, 255, image->g_img->width * image->g_img->height * sizeof(int32_t));
 
 	//mlx_loop_hook(mlx, &hook, mlx);
+	mlx_scroll_hook(image->mlx, &my_scrollhook, image);
 	draw_fractal(image);
 	//exit(0);
 	mlx_image_to_window(image->mlx, image->g_img, 0, 0);
 	mlx_loop(image->mlx);
 	//mlx_delete_image(image->mlx, image->g_img); // clean up once the application requests an exit
-	//mandelbrot();
 	mlx_terminate(image->mlx);
 	return ;
 }
 
-
 /*	MAIN
 *	---------
-* To make use of the MLX42 library and to access all the functions, the correct
-* header needs to be included, then the structure variables must be initialized
+* The structure variables must be initialized
 * to connect the main handle. The init function creates the window.
 */ 
 
