f5c7620994d5e1a01044f843fe5b2a3e3699a146
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-10 15:25:14 +0100

    working version, but still very slow, coloring not working. To be enhanced

--- srcs/draw_fractal.c
+++ srcs/draw_fractal.c
@@ -6,39 +6,49 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 15:45:38 by kczichow          #+#    #+#             */
-/*   Updated: 2022/12/20 15:54:12 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/10 15:07:45 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-/*	DRAW_FRACTOL
+/*	DRAW_FRACTAL
 *	-------------
 *	Central function of the program. The function iterates through each pixel in
-*	the window. It calls the function respectively to apply a coordinate to each
-*	of the pixels, calculate the number of iterations for each point and put
+*	the window. To be able to apply the mandelbrot fomula to the image, for each
+*	pixel the corresponding coordinates on the real and the imaginary axis need
+*	to be calculated.
+ calculate the number of iterations for each point and put
 *	a particular color to it.
 */
 
 void	draw_fractal(t_image *image)
 {
-	int iter;
+	int pix_x;
+	int pix_y;
 	
-	iter = 0;
-	image->pix_x = 0;
-	while (image->pix_x < WIDTH)
+	image->set->iter = 0;
+	pix_y = 0;
+	while (pix_y < HEIGHT)
 	{
-		image->pix_y = 0;
+		pix_x = 0;
+		image->set->c_im = image->set->max_im - pix_y * image->set->coef_im;
 		
-		while (image->pix_y < HEIGHT)
+		while (pix_x < WIDTH)
 		{
-			scale_pixel_to_coordinates(image, image->set);
-			iter = count_iterations(image->set);
-			if (iter < image->set->max_iter)
-				mlx_put_pixel(image->g_img, image->pix_x , image->pix_y, get_rgba(0, 0, 0 , 255));
+			image->set->c_re = image->set->min_re + pix_x * image->set->coef_re;
+			image->set->iter = count_iterations(image->set);
+			//generate_colors()
+			if (image->set->iter < image->set->max_iter)
+			// 	printf("%d\n", iter);
+				mlx_put_pixel(image->g_img, pix_x, pix_y, generate_colors(image->set->iter, image->set));
+			else
+				mlx_put_pixel(image->g_img, pix_x, pix_y, 0xFFFFFFFF);
+				// mlx_put_pixel(image->g_img, image->pix_x , image->pix_y, get_rgba(0, 0, 0));
 			//put_color(image, iter);
-			image->pix_y++;
+			pix_x++;
 		}
-		image->pix_x++;
+		pix_y++;
 	}
+	// mlx_image_to_window(image->mlx, image->g_img, image->pix_x, image->pix_y);
 }
