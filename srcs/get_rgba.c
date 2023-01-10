96442ca60d79229a28fa04f449bc07c7abf6b804
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-10 15:24:14 +0100

    made function more readable, added description, hardcoded byte for transparency

--- srcs/get_rgba.c
+++ srcs/get_rgba.c
@@ -6,13 +6,19 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 10:11:10 by kczichow          #+#    #+#             */
-/*   Updated: 2023/01/09 10:14:49 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/10 15:23:12 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int	get_rgba(int r, int g, int b, int a)
+/*	GET_RGBA
+*	----------
+*	Function combines four individual channel bytes into a single integer using
+*	bit-shifting. The last byte is for transparency and always set to zero.
+*/
+
+uint32_t	get_rgba(uint8_t red, uint8_t green, uint8_t blue)
 {
-	return (r << 24 | g << 16 | b << 8 | a);
+	return ((uint32_t)(red << 24 | green << 16 | blue << 8 | 0));
 }
