c3eca74af19e15020a409b15b5338aabba94d8c7
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-11 14:02:35 +0100

    removed coordinates struct

--- srcs/allocate_memory.c
+++ srcs/allocate_memory.c
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 14:43:44 by kczichow          #+#    #+#             */
-/*   Updated: 2023/01/10 14:38:48 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/11 14:02:00 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,6 @@
 
 void	allocate_memory(t_image *image)
 {	
-	image->coordinates = ft_calloc(1, sizeof(t_coordinates));
-	if (!image->coordinates)
-		return ;
 	image->set = ft_calloc(1, sizeof(t_set));
 	if (!image->set)
 		return ;
