180403051a1b93e662438adaac2ae1712dac4e20
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-11 14:09:09 +0100

    added escape button and clean up function call

---
+++ srcs/keyhook.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keyhook.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/11 10:14:48 by kczichow          #+#    #+#             */
+/*   Updated: 2023/01/11 13:52:32 by kczichow         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+void	my_keyhook(mlx_key_data_t keydata, void *param)
+{
+	mlx_t	*mlx;
+	t_image *image;
+	t_set	*set;
+	
+	image = param;
+	set = image->set;
+	mlx = image->mlx;
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+		clean_up(image);
+}
+
+// oid my_keyhook1(mlx_key_data_t keydata, void *param)
+// {
+// 	// If we PRESS the 'J' key, print "Hello".
+// 	if (keydata.key == MLX_KEY_J && keydata.action == MLX_PRESS)
+// 		puts("Hello ");
+
+// 	// If we RELEASE the 'K' key, print "World".
+// 	if (keydata.key == MLX_KEY_K && keydata.action == MLX_RELEASE)
+// 		puts("World");
+
+// 	// If we HOLD the 'L' key, print "!".
+// 	if (keydata.key == MLX_KEY_L && keydata.action == MLX_REPEAT)
+// 		puts("!");
+	
+// 	if (keydata.key == MLX_KEY_A && keydata.action == MLX_RELEASE && keydata.modifier == MLX_CONTROL)
+// 		puts("Gotta grab it all!");
+// }
