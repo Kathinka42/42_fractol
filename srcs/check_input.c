01520009370389f299f557f5f6bd4a94f0871fea
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-11 14:03:46 +0100

    updated function for Julia and Mandelbrot, included call to clean up function + print instructions

--- srcs/check_input.c
+++ srcs/check_input.c
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 15:02:21 by kczichow          #+#    #+#             */
-/*   Updated: 2022/12/19 17:38:30 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/11 13:55:47 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,22 @@
 /*	CHECK_INPUT
 *	--------------
 *	Function distinguishes between input sets. If Julia, it also checks for
-*	valid input parameters. If input is valid, function returns 0.
+*	valid input parameters. If input is not valid, function calls clean up
+*	function from where it exits the program.
 */
 
 int	check_input(int argc, char **argv, t_image *image)
 {
-	if	(argc < 2 || argc == 3 || argc > 4)
-		return (-1);
-	else if (argc == 2 && ft_strncmp(argv[1], "Mandelbrot", 11))
-		return (-1);
-	printf("argv2: %d\n", param_is_valid(argv[2], image));
-	printf("argv3: %d\n", param_is_valid(argv[3], image));
-	// if (argc == 4 && !ft_strncmp("Julia", argv[1], 6)
-	// 	&& param_is_valid(argv[2], image) && param_is_valid(argv[3], image))
-	// 	return (-1);
+	image->mlx = NULL;
+	if (argc == 2 && !ft_strncmp(argv[1], "Mandelbrot", 11))
+		image->set->settype = 1;
+	else if (argc == 4 && !ft_strncmp(argv[1], "Julia", 6)
+		&& param_is_valid(argv[2]) && param_is_valid(argv[3]))
+		image->set->settype = 2;
+	else
+	{
+		print_instructions();
+		clean_up(image);
+	}
 	return (0);
 }
