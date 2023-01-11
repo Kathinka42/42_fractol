81fb192e3a37df68c75ce8b5077eccddcc3c2042
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-11 14:07:49 +0100

    no changes

--- srcs/get_rgba.c
+++ srcs/get_rgba.c
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 10:11:10 by kczichow          #+#    #+#             */
-/*   Updated: 2023/01/10 15:23:12 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/11 09:54:36 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 /*	GET_RGBA
 *	----------
 *	Function combines four individual channel bytes into a single integer using
-*	bit-shifting. The last byte is for transparency and always set to zero.
+*	bit-shifting. The last byte is for transparency and always set to 255.
 */
 
 uint32_t	get_rgba(uint8_t red, uint8_t green, uint8_t blue)
 {
-	return ((uint32_t)(red << 24 | green << 16 | blue << 8 | 0));
+	return ((uint32_t)(red << 24 | green << 16 | blue << 8 | 255));
 }
