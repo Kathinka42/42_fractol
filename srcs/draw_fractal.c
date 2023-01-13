02e2f8073bf17bf5c896cefa83e049263840b1ee
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-13 14:21:55 +0100

    no changes

--- srcs/draw_fractal.c
+++ srcs/draw_fractal.c
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 15:45:38 by kczichow          #+#    #+#             */
-/*   Updated: 2023/01/11 10:10:47 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/13 13:22:45 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,14 @@ void	draw_fractal(t_image *image)
 {
 	int pix_x;
 	int pix_y;
-	
+
 	image->set->iter = 0;
 	// change_color(image->set);
 	pix_y = 0;
 	while (pix_y < HEIGHT)
 	{
 		pix_x = 0;
-		image->set->c_im = image->set->max_im - pix_y * image->set->coef_im;
-		
+		image->set->c_im = image->set->max_im - pix_y * image->set->coef_im;		
 		while (pix_x < WIDTH)
 		{
 			image->set->c_re = image->set->min_re + pix_x * image->set->coef_re;
