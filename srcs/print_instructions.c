35132262ea449ab938fc353f1a86e41ce602e7ac
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-13 09:47:12 +0100

    cleansed and updated instructions

--- srcs/print_instructions.c
+++ srcs/print_instructions.c
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 11:14:08 by kczichow          #+#    #+#             */
-/*   Updated: 2023/01/11 13:59:31 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/13 09:30:45 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,15 @@
 void	print_instructions(void)
 {
 	write(1, "\n", 1);
-	write(1, "-> Use: ./fractol followed by\n", 31);
+	write(1, "Use: ./fractol followed by\n", 28);
 	write(1, "\n", 1);
-	write(1, "  - [Mandelbrot] for Mandelbrot set\n", 36);
-	write(1, "  - [Julia] [num.num] [num.num] for Julia set\n", 46);
-	write(1, "    [num.num] is a floating point number ('.' - delimeter).\n", 56);
+	write(1, "- [Mandelbrot] for Mandelbrot set\n", 34);
+	write(1, "- [Julia] [num.num] [num.num] for Julia set, where [num.num] is\
+	a floating point number with '.' as delimeter\n", 110);
 	write(1, "\n", 1);
-	write(1, "For [Mandelbrot] NO additional parameters possible\n", 51);
-	write(1, "For [Julia] either none OR 2 parameters possible.\n", 50);
+	write(1, "For [Mandelbrot] no additional parameters possible\n", 51);
+	write(1, "For [Julia] either none or 2 parameters possible\n", 50);
 	write(1, "\n", 1);
-	write(1, "-> Example: ./fractol Mandelbrot\n", 33);
-	write(1, "-> Example: ./fractol Julia −0.4 0.6\n", 39);
+	write(1, "Example: ./fractol Mandelbrot\n", 30);
+	write(1, "Example: ./fractol Julia −0.4 0.6\n", 36);
 }
