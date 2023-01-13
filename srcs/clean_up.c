adbeca07758dff4ef2dfabdc06c5b73e48ce3c9e
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-13 14:22:56 +0100

    added system leaks check function

--- srcs/clean_up.c
+++ srcs/clean_up.c
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 12:57:33 by kczichow          #+#    #+#             */
-/*   Updated: 2023/01/11 13:52:40 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/13 09:46:23 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ void	clean_up(t_image *image)
 {
 	free (image->set);
 	free (image);
-	printf("CLEAN_UP\n");
 	if (image->mlx)
 	{
 		mlx_delete_image(image->mlx, image->g_img);
 		mlx_terminate(image->mlx);
 	}
+	system ("leaks fractol");
 	exit (0);
 }
