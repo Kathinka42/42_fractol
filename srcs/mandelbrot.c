4d4fc5e1c685c578bd725ea8d708a1763cde8460
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-13 15:19:55 +0100

    changed file name and function name to account for settype

--- srcs/mandelbrot.c
+++ srcs/mandelbrot.c
@@ -1,106 +1,56 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mandelbrot.c                                       :+:      :+:    :+:   */
+/*   mandelbrot.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/22 10:52:03 by kczichow          #+#    #+#             */
-/*   Updated: 2022/12/20 12:27:53 by kczichow         ###   ########.fr       */
+/*   Created: 2022/12/19 15:14:09 by kczichow          #+#    #+#             */
+/*   Updated: 2023/01/13 14:58:51 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+/*	CALCULATE_MANDELBROT
+*	-------------------------
+*	Function calculates the next interation of Z, split up into both a real
+*	and an imaginary part.
+*	Basic function is Z(n+1) = Z(n)*Z(n) + C, where Z(n+1) is the next
+*	iteration. Both, Z and C can be split up in imaginary and real parts, e.g.
+*	c = c_imag + c_real.
+*	c_imag and c_real are equal to the point in the coordinating system on which
+*	the iteration is executed and are input parameters to the count iterations
+*	function.
+*/
 
+void	calculate_mandelbrot(t_set *set)
+{
+	double	tmp_im;
 
+	tmp_im = 2 * set->z_re * set->z_im + set->c_im;
+	set->z_re = pow(set->z_re, 2) - pow(set->z_im, 2) + set->c_re;
+	set->z_im = tmp_im;
+}
 
-// the mandelbrot algorithm , for each point starts with zero and add a changing
-// constant
+/*	COUNT_ITERATIONS_MANDELBROT
+*	-----------------------------
+*	Function counts the number of iterations for a particular point in the
+*	coordinating system, for which Z remains within the boundaries of the set.
+*	Z always starts from the center (0,0). This is the major difference to the
+*	Julia set.
+*/
 
-// typedef struct {unsigned char r, g, b;} rgb_t;
-// rgb_t **tex = 0;
-// int gwin;
-// //GLuint texture;
-// int width, height;
-// int tex_w, tex_h;
-// double scale = 1./256;
-// double cx = -.6, cy = 0;
-// int color_rotate = 0;
-// int saturation = 1;
-// int invert = 0;
-// int max_iter = 256;
-
-// https://rosettacode.org/wiki/Mandelbrot_set#C
-
-// void	mandelbrot()
-// {
-// 	int i, j, iter, min, max;
-// 	rgb_t *px;
-// 	double x, y, zx, zy, zx2, zy2;
-// 	min = max_iter; max = 0;
-// 	for (var->i = 0; var->i < height; i++) {
-// 		var->px = tex[var->i];
-// 		y = (i - height/2) * scale + cy;
-// 		for (j = 0; j  < width; j++, px++) {
-// 			x = (j - width/2) * scale + cx;
-// 			iter = 0;
- 
-// 			zx = hypot(x - .25, y);
-// 			if (x < zx - 2 * zx * zx + .25) iter = max_iter;
-// 			if ((x + 1)*(x + 1) + y * y < 1/16) iter = max_iter;
- 
-// 			zx = zy = zx2 = zy2 = 0;
-// 			for (; iter < max_iter && zx2 + zy2 < 4; iter++) {
-// 				zy = 2 * zx * zy + y;
-// 				zx = zx2 - zy2 + x;
-// 				zx2 = zx * zx;
-// 				zy2 = zy * zy;
-// 			}
-// 			if (iter < min) min = iter;
-// 			if (iter > max) max = iter;
-// 			*(unsigned short *)px = iter;
-// 		}
-// 	}
- 
-// 	for (i = 0; i < height; i++)
-// 		for (j = 0, px = tex[i]; j  < width; j++, px++)
-// 			printf("TEST\n");
-// 			//hsv_to_rgb(*(unsigned short*)px, min, max, px);
-// }
-
-// void	mandelbrot()
-// {
-// 	int i, j, iter, min, max;
-// 	rgb_t *px;
-// 	double x, y, zx, zy, zx2, zy2;
-// 	min = max_iter; max = 0;
-// 	for (i = 0; i < height; i++) {
-// 		px = tex[i];
-// 		y = (i - height/2) * scale + cy;
-// 		for (j = 0; j  < width; j++, px++) {
-// 			x = (j - width/2) * scale + cx;
-// 			iter = 0;
- 
-// 			zx = hypot(x - .25, y);
-// 			if (x < zx - 2 * zx * zx + .25) iter = max_iter;
-// 			if ((x + 1)*(x + 1) + y * y < 1/16) iter = max_iter;
- 
-// 			zx = zy = zx2 = zy2 = 0;
-// 			for (; iter < max_iter && zx2 + zy2 < 4; iter++) {
-// 				zy = 2 * zx * zy + y;
-// 				zx = zx2 - zy2 + x;
-// 				zx2 = zx * zx;
-// 				zy2 = zy * zy;
-// 			}
-// 			if (iter < min) min = iter;
-// 			if (iter > max) max = iter;
-// 			*(unsigned short *)px = iter;
-// 		}
-// 	}
- 
-// 	for (i = 0; i < height; i++)
-// 		for (j = 0, px = tex[i]; j  < width; j++, px++)
-// 			printf("TEST\n");
-// 			//hsv_to_rgb(*(unsigned short*)px, min, max, px);
-// }
+int	count_iterations_mandelbrot(t_set *set)
+{
+	set->iter = 0;
+	set->z_re = 0.0;
+	set->z_im = 0.0;
+	while (set->iter < set->max_iter
+			&& (pow(set->z_re, 2) + pow(set->z_im, 2)) < 4.0)
+	{
+		calculate_mandelbrot(set);
+		set->iter++;
+	}
+	return (set->iter);
+}
